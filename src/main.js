a5063b9a0c9a86ca238468c444c35dfd699bb721
Leon <410763533@qq.com>
2020-10-15 18:05:59 +0800

    dom1

---
+++ src/main.js
@@ -0,0 +1,10 @@
+const div = dom.find('#test>.red')[0] // 获取对应的元素
+console.log(div)
+
+const divList = dom.find('.red') // 获取多个 div.red 元素
+console.log(divList)
+
+dom.each(divList, (n)=> console.log(n)) // 遍历 divList 里的所有元素
+
+dom.style(div, 'color', 'blue') // 设置 div.style.color
+console.log(dom.style(div, 'color'))
